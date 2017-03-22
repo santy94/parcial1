c263f08cb10f61507043e8ae84fa354198b12bb8
santy <santy>
2017-03-22 08:15:31 -0500

    se creo el archivo

---
+++ Tallerdos.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tallerdos;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author santy
+ */
+public class Tallerdos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
